6ed3bbcac6e90bfbb63b80ac1a226b01f61d5e20
Mona <monaneweir212@gmail.com>
2023-03-30 04:22:21 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#
+#include <stdio.h>
+c
+c char *_strcat(char *dest, char *src);
+c char *_strncat(char *dest, char *src, int n);
+i char *_strncpy(char *dest, char *src, int n);
+v int _strcmp(char *s1, char *s2);
+c void reverse_array(int *a, int n);
+c char *string_toupper(char *);
+c char *cap_string(char *);
+c char *leet(char *);
+v char *rot13(char *);
+c void print_number(int n);
+v  char *infinite_add(char *n1, char *n2, char *r, int size_r);
+# void print_buffer(char *b, int size);
+# endit
