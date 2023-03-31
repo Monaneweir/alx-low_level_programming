ac68c1b58ad0b65eaeef57f9ef9037737a10037d
Mona <monaneweir212@gmail.com>
2023-03-31 05:43:08 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,28 +1,76 @@
-include "main.h"
+include <stdio.h>
+#include <ctype.h>
 
 /**
- * leet - transform to leet
- * @s: char array string type
- * Return: s transformed
+ * print_formated_buffer - print a formated 10 bytes or less buffer
+ * @n: the line index
+ * @buffer: the buffer to print @size bytes from
+ * @size: the number of bytes to print, must be 10 or less
  */
-
-char *leet(char *s)
+void print_formated_buffer(int n, char *buffer, int size)
 {
-	int i, j;
-	char s1[] = "aeotl";
-	char s2[] = "AEOTL";
-	char s3[] = "43071";
+	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	/* print index */
+	printf("%08x: ", n);
+
+	/* print bytes in hexdec */
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 5; j++)
-		{
-			if (s[i] == s1[j] || s[i] == s2[j])
-			{
-				s[i] = s3[j];
-				break;
-			}
-		}
+		/* is bytes over, for the final line */
+		int over = i >= size;
+		/* character to print */
+		char c = over ? 0 : *(buffer + i);
+
+		/* if over print white space else char in hex form with 2 zeros padding */
+		if (!over)
+			printf("%02x", c);
+		else
+			printf("  ");
+
+		/* puts white space char every 2 bytes */
+		if (i % 2 == 1)
+			putchar(' ');
 	}
-	return (s);
+
+	/* print bytes as characters */
+	for (i = 0; i < size; i++)
+	{
+		char c = *(buffer + i);
+
+		putchar(isprint(c) ? c : '.');
+	}
+	putchar('\n');
+}
+
+/**
+ * print_buffer - print @b of @size bytes in formatted form
+ * @b: the buffer reference to print
+ * @size: the number of bytes to format and print
+ * Description:
+ * print every 10bytes of @b at a single line,
+ * starting the line with the first character index in the buffer
+ * in hex form padded with 8 zeros.
+ * then the 10bytes in hex forms padded with 2 zeros and a space char
+ * every 2bytes. finally print the 10 printable characters.
+ * if the character is not printable then prints a `.` character instead.
+ */
+void print_buffer(char *b, int size)
+{
+	int i;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < size; i += 10)
+	{
+		int r = size - i;
+
+		print_formated_buffer(i, b, r < 10 ? r : 10);
+		if (r > 10)
+			b += 10;
+	}
 }
