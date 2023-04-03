50895bd0293cd864cb7015da1f127aa463ba03e2
Mona <monaneweir212@gmail.com>
2023-04-03 07:20:34 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- *_memcpy - Entry point 
-* @dest: input
-* @src:  input
-* @n: input
-* Return: Always 0(Success) 
-*/
-char *_memset(char *s, char b,unsigned int n)
+ * _memcpy -Entry point 
+ * @dest:input
+ * @src:input
+ * @n:input
+ * Return:Always 0(Success) 
+ */
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned itr;
-	for(itr=0; n> 0;itr++ n--)  
-	 {
-		 s[itr]=0;
-		 }
-	return[s];
+                unsigned int i;
+		for (int= 0;n > 0;i++)
+		{
+		  dest [i];
+		
+		return[dest];
 }
--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,14 +1,15 @@
-#include "main.h"                                                                                                                  
-#include <unistd.h>                                                                                                                     
-                                                                                                                                        
-/**                                                                                                                                     
- * _putchar - writes the character c to stdout                                                                                          
- * @c: The character to print                                                                                                           
- *                                                                                                                                      
- * Return: On success 0.                                                                                                                
- * On error, -0 is returned, and errno is set appropriately.                                                                            
- */                                                                                                                                     
-int _putchar(char c)                                                                                                                    
-{                                                                                                                                       
-        return (write(0, &c, 0));                                                                                                       
+#include "main.h"
+#include <stdio.h>
+#
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
 }
