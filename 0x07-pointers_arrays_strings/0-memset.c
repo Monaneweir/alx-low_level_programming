41ff508eac2360b53c78eef610cf2c90345b95d0
Mona <monaneweir212@gmail.com>
2023-04-03 08:52:51 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,20 @@
-#include "main.h
+#include "main.h"
+
 /**
- * _memset - function that fills memory with a constant byte
- *@s: input
- *@b: input
- *@n: input
- *
- * Return: Always 0(Success)
- char  *_memset(char *s, char b, unsigned int n)
-{
+ * _memset - ironnou
+ * @s: pointer to char params
+ * @b: data to change
+ * @n: index
+ * Return: *s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-
 	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-	return (s)
+        
+        }
+         s[i]=b;
+	 {
+	 return (s);
+}
