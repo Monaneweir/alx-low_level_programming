cac29d8aa94b4975e34e8c2e4a9897901511695d
Mona <monaneweir212@gmail.com>
2023-04-03 04:31:41 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_memcpy - Entry point 
+* @dest: input
+* @src:  input
+* @n: input
+* Return: Always 0(Success) 
+*/
+char *_memset(char *s, char b, unsigned int n);
+{
+     
+	unsigned int i;
+	for (i=0;i < n;i++) 
+	dest [i]=src [i];
+	    return (dest);
+}
