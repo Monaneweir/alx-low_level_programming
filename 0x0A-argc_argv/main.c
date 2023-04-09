319eee52b4fdeb518dbdd0763f277653fb12bcda
Mona <monaneweir212@gmail.com>
2023-04-09 05:47:08 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/main.c
+++ 0x0A-argc_argv/main.c
@@ -2,6 +2,7 @@
 
 int main(void)
 {
-puts("\"At the end of the day my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+puts("/"At the end of the day my goal was to be the best hacker \"/n/t kevinMitnick");
+
 return (0);
 }
