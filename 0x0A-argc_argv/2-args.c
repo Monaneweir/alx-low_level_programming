dee0600ef94ec51d27717608cd50a8d9745d7c7d
Mona <monaneweir212@gmail.com>
2023-04-09 19:46:51 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,7 +5,6 @@
  * main - prints all arguments it receives
  * @argc: n args
  * @argv: arr args
- * 
  * Return: 0
  */
 
@@ -15,7 +14,7 @@ int main(int argc, char *argv[])
 	
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", argv[i]);
+	   printf("%s\n", argv[i]);
 	}
 
 	return (0);
