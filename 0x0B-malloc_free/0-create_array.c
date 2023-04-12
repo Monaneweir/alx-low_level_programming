44b3bd363cd2f080930405887faa9469531d6af2
Mona <monaneweir212@gmail.com>
2023-04-12 05:49:32 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,26 +1,33 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 
 /**
- * create_array - Entry point
- *@size: size of the array
- *@c: character
- * Return: a pointer to the array, or NULL if it fails
- */
+ * *create_array - creates an array of chars, and initializes it
+ * with a specific char
+ * @size: size of the array
+ * @c: char to be initialized with
+ * Return: pointer
+ **/
 char *create_array(unsigned int size, char c)
 {
-	char *array = NULL;
-	unsigned int i;
+char *str;
+unsigned int i = 0;
 
-	if (size == 0)
-		return (NULL);
-	if (size != 0)
-	{
-		array = (char *)malloc(size * sizeof(char));
-		if (array != NULL)
-		{
-			for (i = 0; i < size; i++)
-				array[i] = c;
-		}
-	}
-	return (array);
+if (size == 0)
+return (NULL);
+
+str = (char *)malloc(size * sizeof(char));
+
+if (str == 0)
+{
+return (NULL);
+}
+
+else
+while (i < size)
+{
+str[i] = c;
+i++;
+}
+return (str);
 }
--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,28 +3,30 @@
 
 
 /**
- * _strdup - Entry point
- *@str: string we need to duplicate
- * Return: a pointer to the duplicated string or NULL
+ * _strdup - copy
+ * @str: pointer to string
+ * Return: pointer
  */
 char *_strdup(char *str)
 {
-	char *strnew = NULL;
-	unsigned int i;
-	int n;
+char *strDup;
+int i, j;
 
-	if (str == NULL)
-		return (NULL);
-	for (n = 0; str[n] != '\0'; n++)
-	;
-	strnew = (char *)malloc(n + 1 * sizeof(char));
-	if (strnew != NULL)
-	{
-		for (i = 0; str[i] != '\0'; i++)
-			strnew[i] = str[i];
-	} else
-	{
-		return (NULL);
-	strnew[i] = '\0';
-	return (strnew);
+if (str == NULL)
+return (NULL);
+i = 0;
+while (str[i] != '\0')
+i++;
+i++;
+strDup = malloc(sizeof(*str) * i);
+if (strDup == NULL)
+return (NULL);
+j = 0;
+while (str[j] != '\0')
+
+{
+strDup[j] = str[j];
+j++;
+}
+return (strDup);
 }
