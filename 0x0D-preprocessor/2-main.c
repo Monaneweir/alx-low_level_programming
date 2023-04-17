b2a0e8a05a7d225ae7c23d23a0693a5d5414c142
Mona <monaneweir212@gmail.com>
2023-04-17 08:31:00 -0700

    a

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,11 +2,11 @@
 
 /**
  * main -prints the name of the file it was compiled from
- *
  * Return Always 0.
  */
-int main (void)  
-print("%s\n", _BASE_FILE_);
-return (0):
+int main (void)
+
+print("%s\n"_FILE_);
+return (0);
 }
 
