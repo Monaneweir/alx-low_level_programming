259f316d89f33cbc8118c8858be255eb83a1aa3d
Mona <monaneweir212@gmail.com>
2023-04-19 08:17:14 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -4,21 +4,24 @@
 /**
  * print_name_as_is - prints a name as is
  * @name: name of the person
+ *
  * Return: Nothing.
  */
 void print_name_as_is(char *name)
 {
-printf("Hello, my name is %s\n", name);
+    printf("Hello, my name is %s\n", name);
 }
 
 /**
  * print_name_uppercase - print a name in uppercase
  * @name: name of the person
+ *
  * Return: Nothing.
  */
 void print_name_uppercase(char *name)
 {
 unsigned int i;
+
 printf("Hello, my uppercase name is ");
 i = 0;
 while (name[i])
@@ -32,10 +35,12 @@ else
 putchar(name[i]);
 }
 i++;
-
 }
+}
+
 /**
  * main - check the code
+ *
  * Return: Always 0.
  */
 int main(void)
