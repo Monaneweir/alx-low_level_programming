e1a771a844159c8872c617854ac8c15ce17d9b9f
Mona <monaneweir212@gmail.com>
2023-04-27 14:09:27 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,19 +1,26 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
-  * print_listint - prints all elements of a list_t list
-  * @h: singly linked list to print
-  * Return: number of nodes in the list
-  */
-size_t print_listint(const listint_t *h)
-{
-	size_t i;
+ * print_list - Prints elements of a list
+ * @h: The list
+ * Return: Number of nodes
+ */
 
-	for (i = 0; h; i++)
+size_t print_list(const list_t *h)
+{
+	size_t node = 0;
+
+	while (h)
 	{
-		printf("%d\n", h->n);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		node++;
 		h = h->next;
 	}
-	return (i);
+
+	return (node);
 }
