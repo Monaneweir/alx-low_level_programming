a870c14fa160365b8dd75cf912e060ceaf2fbc98
Mona <monaneweir212@gmail.com>
2023-04-27 14:16:01 -0700

    a

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,14 +1,20 @@
 #include "lists.h"
-/**
-  * listint_len - gets the number of nodes in a list
-  * @h: singly linked list to print
-  * Return: number of nodes in the list
-  */
-size_t listint_len(const listint_t *h)
-{
-	size_t i;
 
-	for (i = 0; h; i++)
+/**
+ * list_len- Returns the numbers of elements in a linked list
+ * @h: The linked list
+ * Return: Number of elements in a linked list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t num_of_elements = 0;
+
+	while (h)
+	{
+		num_of_elements++;
 		h = h->next;
-	return (i);
+	}
+
+	return (num_of_elements);
 }
