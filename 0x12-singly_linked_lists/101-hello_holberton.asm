583c1287663810a6b19098181cf9374b507d6fc9
Mona <monaneweir212@gmail.com>
2023-04-28 05:19:29 -0700

    a

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,42 +1,9 @@
-#include "lists.h"
-#include <stdio.h>
-
-/**
-* print_listint_safe - function that prints a listint_t linked list.
- * @head: pointer to head of a list.
- * Return: Length of list(INT)
-/*
-size_t print_listint_safe(const listint_t *head)
-{
-const listint_t *slow_p = head ,*fast_p = head;
-size_t ele = 0;
-int is_loop = 0;
-while (slow_p && fast_p && fast_p->next) 
-{
-	if (!(fast_p->next->next))
-			break;
-slow_p = slow_p->next;
-fast_p = fast_p->next->next
-if (slow_p == fast_p)
-{
-slow_p = slow_p->next;
-is_loop = 1;
-break;
-}
-}
-if (!is_loop)
-{
-while (head)
-ele++;
-if (head == slow_p)
-	{
-printf("[%p] %d\n", (void *)head, head->n);
-printf("-> [%p] %d\n", (void *)head, head->next->n);
-exit(98);
-}
-
-printf("[%p] %d\n", (void *)head, head->n);
-head = head->next;
-}
-return (0);
-}
+global main
+extern printf
+main:
+movedi, format
+xor eax, eax
+call printf
+mov eax,0
+ret
+format:db 'Hello,Holberton\n',0
