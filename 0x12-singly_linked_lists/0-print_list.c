5b591fd85937fddaec737e2ca65ce24c943ff483
Mona <monaneweir212@gmail.com>
2023-04-28 06:37:10 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,14 @@
 
 size_t print_list(const list_t *h)
 {
-size_t n=0:
-while (h) 
-{
+size_t n = 0;
+while (h)
 if (!h->str)
 printf("[0] (nil)\n");
 else
-printf("[%u] %s\n" , h->len, h->str);
-h =h_>next;
-n++
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+n++;
 }
 return (n);
 }
