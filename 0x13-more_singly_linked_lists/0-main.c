e11479d83b53da7d5fc8908124d885c06d800d30
Mona <monaneweir212@gmail.com>
2023-04-29 12:46:34 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-main.c
+++ 0x13-more_singly_linked_lists/0-main.c
@@ -5,7 +5,7 @@
 
 /**
  * main - check the code
- * 
+ *
  * Return: Always 0.
  */
 int main(void)
@@ -14,7 +14,6 @@ listint_t *head;
 listint_t *new;
 listint_t hello = {8, NULL};
 size_t n;
-
 head = &hello;
 new = malloc(sizeof(listint_t));
 if (new == NULL)
