0c6dbfcd6b1c0c8f47e23b8b06eea39b98b25174
Mona <monaneweir212@gmail.com>
2023-04-29 03:03:23 -0700

    101-wildcmp.c

--- 0x08-recursion/101-main.c
+++ 0x08-recursion/101-main.c
@@ -2,14 +2,15 @@
 #include <stdio.h>
 
 /**
-* main - check the code
-*
-* Return: Always 0.
+ * main - check the code
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int r;
-	r = wildcmp("main.c", "*.c");
+    int r;
+
+    r = wildcmp("main.c", "*.c");
     printf("%d\n", r);
     r = wildcmp("main.c", "m*a*i*n*.*c*");
     printf("%d\n", r);
