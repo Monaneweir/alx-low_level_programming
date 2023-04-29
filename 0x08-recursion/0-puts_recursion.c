1a295924a1da67bd7c6586aae48a56084d66a8ef
Mona <monaneweir212@gmail.com>
2023-04-29 01:58:13 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,14 +5,18 @@
  * @s: pointer to the string
  * Return: void
  */
+
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-	 _putchar('\n');
-        }
-	else
-        {
-	  _putchar(*s);
-	  _puts_recursion(s + 1);
-	}
+
+if (*s == '\0')
+{
+_putchar('\n');
+}
+else
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+
+}
